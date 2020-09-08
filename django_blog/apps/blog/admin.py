2a11462ca4517d80b95c70dd50ff39b0cfda89a4
francobustos <franco.bustos60@gmail.com>
2020-09-08 13:42:39 -0300

    Añadi boton import y export en el admin en author y category

--- django_blog/apps/blog/admin.py
+++ django_blog/apps/blog/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryResource(resources.ModelResource):
+    class Meta:
+        model = Category
+
+class CategoryAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     search_fields = ['name']
     list_display = ('name', 'state', 'creation_date',)
+    resource_class = CategoryResource
 
-class AuthorAdmin(admin.ModelAdmin):
+class AuthorResource(resources.ModelResource):
+    class Meta:
+        model = Author
+
+class AuthorAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     search_fields = ['names', 'last_names', 'email']
     list_display = ('names', 'last_names', 'email', 'state', 'creation_date',)
+    resource_class = AuthorResource
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Author, AuthorAdmin)
