efdba127d3af41212c954afd2f9c0d906c4fb99d
francobustos <franco.bustos60@gmail.com>
2020-09-10 16:34:07 -0300

    Mejoras en el views.py

--- django_blog/apps/blog/views.py
+++ django_blog/apps/blog/views.py
@@ -1,40 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
+
 
 def home(request):
     posts = Post.objects.filter(state = True)
     return render(request, 'index.html',{'posts':posts})
 
 def detailsPost(request,slug):
-    post = Post.objects.get(
-        slug = slug
-    )
+    post = get_object_or_404(Post, slug=slug)
     return render(request, 'post.html', {'details_post': post})
 
 def generals(request):
     posts = Post.objects.filter(
         state = True,
-        category = Category.objects.get(name = 'General')
+        category = Category.objects.get(name__iexact = 'General')
     )
     return render(request, 'generals.html',{'posts':posts})
 
 def anime(request):
     posts = Post.objects.filter(
         state = True,
-        category = Category.objects.get(name = 'Anime')
+        category = Category.objects.get(name__iexact = 'Anime')
     )
     return render(request, 'anime.html',{'posts':posts})
 
 def piano(request):
     posts = Post.objects.filter(
         state = True,
-        category = Category.objects.get(name = 'Piano')
+        category = Category.objects.get(name__iexact = 'Piano')
     )
     return render(request, 'piano.html',{'posts':posts})
 
 def literature(request):
     posts = Post.objects.filter(
         state = True,
-        category = Category.objects.get(name = 'Literatura')
+        category = Category.objects.get(name__iexact = 'Literatura')
     )
     return render(request, 'literature.html',{'posts':posts})
